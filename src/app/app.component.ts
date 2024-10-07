3e9c9424cddad349671817b2bb7f7e4eaab443a5
Vinicius Leal da Silva <Vinicius.leal@divinaprovidencia.org.br>
2024-10-07 16:26:50 -0300

    4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
-import { and } from "@angular/router/esm2015/src/utils/collection";
 import { SwPush, SwUpdate } from "@angular/service-worker";
 
 @Component({
